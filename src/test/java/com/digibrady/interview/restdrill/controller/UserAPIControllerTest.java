df9878f9c09e046a3989c3bfe4ec7ac058454934
Bob Brady <bob@digibrady.com>
2016-12-15 11:05:38 -0500

    Updated tests to loop over ResultActions builder

--- src/test/java/com/digibrady/interview/restdrill/controller/UserAPIControllerTest.java
+++ src/test/java/com/digibrady/interview/restdrill/controller/UserAPIControllerTest.java
@@ -12,6 +12,7 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -39,20 +40,32 @@ public class UserAPIControllerTest {
 	public void getAllUsers() throws Exception {
 		List<User> mockUsers = createMockUsers(10);
 		Mockito.when(mockUserRepo.getUsers()).thenReturn(mockUsers);
-		mockMvc.perform(MockMvcRequestBuilders.get("/api/user"))
+
+		ResultActions actions = mockMvc.perform(MockMvcRequestBuilders.get("/api/user"))
 		    .andExpect(MockMvcResultMatchers.status().isOk())
 		    .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
-		    .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(10)))
-		    .andExpect(MockMvcResultMatchers.jsonPath("$[0].id", Matchers.is(1)));
+		    .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(10)));
+
+		assertMockUsers(actions, mockUsers);
 	}
 
 	private List<User> createMockUsers(int count) {
 		List<User> users = new ArrayList<>();
 		for (int idx = 1; idx <= count; idx++) {
-			User user = new User(idx, "name-" + idx, "job", idx * 40);
+			double salary = idx * 40;
+			User user = new User(idx, "name-" + idx, "job-" + idx, salary);
 			users.add(user);
 		}
 		return users;
+	}
+
+	private void assertMockUsers(ResultActions actions, List<User> mockUsers) throws Exception {
+		for (int idx = 0; idx < mockUsers.size(); idx++) {
+			double salary = (idx + 1) * 40;
+			actions.andExpect(MockMvcResultMatchers.jsonPath("$[" + idx + "].id", Matchers.is(idx + 1)));
+			actions.andExpect(MockMvcResultMatchers.jsonPath("$[" + idx + "].job", Matchers.is("job-" + (idx + 1))));
+			actions.andExpect(MockMvcResultMatchers.jsonPath("$[" + idx + "].salary", Matchers.is(salary)));
+		}
 	}
 
 }
